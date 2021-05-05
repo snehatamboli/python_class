af0a300388572abeea1c1f73a13294697056dbae
snehatamboli <83708349+snehatamboli@users.noreply.github.com>
2021-05-05 18:21:24 +0530

    Add files via upload

---
+++ Day_2.py
@@ -0,0 +1,41 @@
+name = 'Harsh'
+age = 23
+salary = 23500.90
+married = True
+
+name = str(name)
+print(type(name),name)
+name = bool(name)
+print(type(name),name)
+name = float(name)
+print(type(name),name)
+name = int(name)
+print(type(name),name)
+
+age = str(age)
+print(type(age),age)
+age = int(age)
+print(type(age),age)
+age = float(age)
+print(type(age),age)
+age = bool(age)
+print(type(age),age)
+
+salary = str(salary)
+print(type(salary),salary)
+salary = bool(salary)
+print(type(salary),salary)
+salary = float(salary)
+print(type(salary),salary)
+salary = int(salary)
+print(type(salary),salary)
+
+married = str(married)
+print(type(married),married)
+married = bool(married)
+print(type(married),married)
+married = float(married)
+print(type(married),married)
+married = int(married)
+print(type(married),married)
+
