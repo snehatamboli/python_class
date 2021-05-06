7730a131e2f5068c69c74bf06b550a2c0187059b
snehatamboli <83708349+snehatamboli@users.noreply.github.com>
2021-05-06 13:18:31 +0530

    Add files via upload

---
+++ Day_3.py
@@ -0,0 +1,7 @@
+#age calculator
+year_of_birth = int(input("Enter the year of birth = "))
+current_year = int(input("Enter the current year = "))
+
+Age = current_year - year_of_birth
+
+print("your current age is = ",Age)
---
+++ Day_3second.py
@@ -0,0 +1,17 @@
+#simple calculator
+num1 = int(input())
+num2 = int(input())
+add= num1 + num2
+subtract= num1-num2
+divide= num1/num2
+multiply = num1*num2
+
+print("Addition = ",add)
+print("Substraction =",subtract)
+print("Division = ",divide)
+print("Multiplication = ",multiply)
+
+
+
+
+
